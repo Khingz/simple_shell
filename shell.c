d36ab98455fb24aceaca39712e6f1c81e562ff28
Amadi <whytes2256@gmail.com>
2023-05-16 13:11:27 -0700

    updating..

--- shell.c
+++ shell.c
@@ -1,4 +1,38 @@
-int main (void)
+#include "shell.h"
+
+
+/**
+ * main - program that run a simple UNIX command for an interpreter.
+ * @argc: arguement count.
+ * @argv: arguement vector.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
 {
-	return 0;
+	char *prmt;
+
+	prmt = "$ ";
+
+	if (argc != 1)
+	{
+		continue;
+
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 2);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
+		{
+			if (ret == END_OF_FILE)
+				write(STDOUT_FILENO, new_line, 1);
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
+		}
+	}
+
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
 }
